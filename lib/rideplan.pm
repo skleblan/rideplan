1986b3e716360c834435a9c9bc1ce0fafdbb3685
Steven LeBlanc <skleblan@gmail.com>
2021-05-21 14:24:38 -0500

    templogin. no passwd. session. untested

--- lib/rideplan.pm
+++ lib/rideplan.pm
@@ -23,11 +23,28 @@ use Dancer2;
 
 my $footlinks = [
   { name => "Home", link => uri_for('/') },
-  { name => "Dashboard", link => uri_for('/dashboard') }
+  { name => "Dashboard", link => uri_for('/dashboard') },
+  { name => "Login", link=>uri_for('/templogin') },
+  { name => "Logout", link=>uri_for('/templogout') }
+
 ];
 
 get '/' => sub {
   template('index', { title=>'RidePlan Home', footlinks => $footlinks });
+};
+
+get '/templogin' => sub {
+  template('templogin', { title => 'Login', loginurl => uri_for('/templogin'), footlinks => $footlinks } );
+};
+
+post '/templogin' => sub {
+  session 'username' => body_parameters->get('username');
+  redirect uri_for('/dashboard');
+};
+
+post '/templogout' => sub {
+  app->destroy_session;
+  redirect uri_for('/');
 };
 
 get '/dashboard' => sub {
@@ -36,6 +53,7 @@ get '/dashboard' => sub {
       { name => 'Flint Hills Ride' },
       { name => 'Kevins bday' }
     ],
+    username => session('username'),
     footlinks => $footlinks
   };
   template('dashboard', $data);
