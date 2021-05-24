55a176466f84c123b1647941b78c6c3cd8440bfc
Steven LeBlanc <skleblan@gmail.com>
2021-05-24 16:37:18 -0500

    basic db module. seperated but working

--- lib/db.pm
+++ lib/db.pm
@@ -42,9 +42,9 @@ sub init
     return;
   }
   $self->dbd_type($type);
-  my $dbh = DBI::connect("dbi:SQLite:$filepath", "", "") or croak $DBI::errstr;
+  my $dbh = DBI->connect("dbi:SQLite:dbname=$filepath", "", "") or croak $DBI::errstr;
   $self->handle($dbh);
-  $self->is_initialized = 1;
+  $self->is_initialized(1);
 }
 
 sub get_ride_data
@@ -55,8 +55,15 @@ sub get_ride_data
   my $sql = "select id, name, miles, regionloc from ride";
 
   my $sth = $self->handle->prepare($sql) or croak $self->handle->errstr;
-  $sth->execute;
+  $sth->execute or croak $sth->errstr;
 
+  my $retval = [];
+  my $row = {};
+  while($row = $sth->fetchrow_hashref)
+  {
+    push @$retval, $row;
+  }
+  return $retval;
 }
 
 1;
