85e2a9d11ad6b14def60d928e0b60c5bf33b6982
Steven LeBlanc <skleblan@gmail.com>
2021-08-19 11:26:14 -0500

    Mock oauth token route. Refine account auth route.
    Added some required parameters for the original account
    auth route.  Even thought the mock service isn't using them,
    google will.
    
    Implemented route to retreive API token given an auth code.
    
    Add some more notes.  One more service needs to be mocked up.

--- util/mock_auth_server.pl
+++ util/mock_auth_server.pl
@@ -28,21 +28,42 @@ my $html = "<html><body>Login in using google account \"lisa\"?".
   "<a href=\"/accept\">accept</a><a href=\"decline\">decline</a>".
   "</body></html>";
 
-#NOTE: this utility emulates 2 separate hosts
+#NOTE: this utility emulates 3 separate hosts
   #accounts.google.com
   #oauth2.googleapis.com
+  #currently unknown profile service url
+
+#google has an "OAuth 2.0 Playground" for testing
+#there is a link in the "Web Server: OAuth 2.0" developer documentation page
+
+# API name: Google OAuth2 API v2
+# https://www.googleapis.com/auth/userinfo.email
+# https://www.googleapis.com/auth/userinfo.profile
 
 get '/o/oauth2/v2/auth' => sub {
-  for my $param (('state', 'redirect_uri', 'client_id'))
+  my @params_needed = ('state', 'redirect_uri', 'client_id', 'response_type',
+    'scope', 'access_type');
+  for my $param (@params_needed)
   {
-    session $param => query_parameters->get($param);
+    my $temp = query_parameters->get($param);
+    if($temp)
+    {
+      session $param => $temp;
+    }
+    else
+    {
+      send_error("Missing params", 400);
+    }
   }
 
   return $html;
 };
 
 get '/accept' => sub {
-  redirect session('redirect_uri'), { code => '4/P7q7W91a-oMsCeLvIaQm6bTrgtp7' };
+  redirect session('redirect_uri'), {
+    code => '4/P7q7W91a-oMsCeLvIaQm6bTrgtp7',
+    state => session('state')
+  };
 };
 
 get '/decline' => sub {
@@ -50,7 +71,18 @@ get '/decline' => sub {
 };
 
 post '/token' => sub {
-  send_error("Not implemented", 500);
+  my $code = body_parameters->get('code');
+  my $redirect = body_parameters->get('redirect_uri');
+
+  my $retval = {
+    'access_token' => '1/fFAGRNJru1FTz70BzhT3Zg',
+    'expires_in' => 3920,
+    'token_type' => 'Bearer',
+    'scope' => 'https://www.googleapis.com/auth/drive.metadata.readonly',
+    'refresh_token' => '1//xEoDL4iW3cxlI7yDbSRFYNG01kVKM2C-259HOF2aQbI'
+  };
+
+  return encode_json($retval);
 };
 
 dance;
